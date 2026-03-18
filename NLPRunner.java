53743993855cd0f8b939b282586d257ca6d5ec16
danica | 대니카 <xxdanicalittlexx@gmail.com>
2026-03-17 19:47:57 -0700

    Create NLPRunner.java

---
+++ NLPRunner.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class NLPRunner {
+  public static void main(String[] args) {
+
+    // Instantiate a MyNLP object ("student-developed class")
+    myNLP nlp = new myNLP();
+
+    // Calls prompt() method to collect user input (optional)
+    nlp.prompt();
+
+    // Prints the summary/result of the NLP project
+    
+  }
+}
